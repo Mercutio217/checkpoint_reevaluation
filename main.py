108a0a670c05183da3df45d914d90ea0d1d6ca8c
Łukasz <l.bielenin@gmail.com>
2017-05-04 17:53:08 +0200

    Refactor main: adding url_for

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from controller.Product_controller import Product_Controller
 from model.Product import Product
 
@@ -16,14 +16,14 @@ def add_product():
     if request.method == "GET":
         return render_template("add_product.html")
     else:
-        new_product = Product(request.form("name"), request.form("description"), request.form("price"))
+        new_product = Product(request.form["name"], request.form["description"], request.form["price"])
         my_controller.add_product(new_product)
-        return render_template("index.html", message="Product added succesfully!")
+        return redirect(url_for("index"))
 
-@app.route("/remove/<int:product_id>", methods=["POST"])
+@app.route("/remove/<int:product_id>")
 def remove_product(product_id):
     my_controller.remove_product(product_id)
-    return render_template("index.html", message="Product was succesfully removed!")
+    return redirect(url_for("index"))
 
 
 
